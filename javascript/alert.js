6ac82dd70c02f7159d125905698154a5fd84cf75
hchsueh <hchsuehgm@gmail.com>
2015-11-22 08:46:16 +0800

    add alert

---
+++ javascript/alert.js
@@ -0,0 +1,7 @@
+ $(window).resize(function() {
+
+if ($(window).width() < 1025) {
+   alert("This site doesn't work under this device/window size. Please switch to laptop/desktop or enlarge window for better website experience.");
+}
+
+});
