966ff9555dbf77b0dad45d66192c2ed2bf0a2fef
hchsueh <hchsuehgm@gmail.com>
2015-12-10 18:57:58 +0800

    updated script&menu width

--- javascript/alert.js
+++ javascript/alert.js
@@ -1,7 +1,22 @@
- $(window).resize(function() {
+
+window.onload = function(){
 
 if ($(window).width() < 1025) {
-   alert("This site doesn't work under this device/window size. Please switch to laptop/desktop or enlarge window for better website experience.");
+   //alert("This site doesn't work under this device/window size. Please switch to laptop/desktop or enlarge window for better website experience.");
+}
+
+}
+
+
+
+$(window).resize(function() {
+
+if ($(window).width() < 1025) {
+   //alert("[on resize] This site doesn't work under this device/window size. Please switch to laptop/desktop or enlarge window for better website experience.");
 }
 
 });
+
+//$(document).ready(function(){
+
+
